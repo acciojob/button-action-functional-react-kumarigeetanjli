c4d304e86a8bc0d8468709f905297b96a579b84e
kumarigeetanjli <84951879+kumarigeetanjli@users.noreply.github.com>
2023-01-12 16:09:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const[paragraph,setPara]=useState(false);
+  const Btn = () =>{
+    setPara(!paragraph)
+  };
   return (
     <div id="main">
-      // Do not alter the main div
+      {paragraph ?  "":<p className="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+      <button className="click" onClick={Btn}>Paragraph</button>
     </div>
   );
 }
